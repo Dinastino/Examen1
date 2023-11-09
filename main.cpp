9a03b0ea1d102c2d67208ebdbe0f7370074085cf
Carlos <corammun@myuax.com>
2023-11-09 11:48:19 +0100

    1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+
+int ej1(){
+    int a, n;
+    std::cout << "Escriba un numero: \n";std::cin >> n;
+    std::cout << "Escriba un numero: \n";std::cin >> a;
+    return a + n;
+}
+
+int main(){
+    std::cout << ej1() << std::endl;
+}
