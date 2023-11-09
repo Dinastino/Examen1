358589df577f3dd31e647f44d98ef4a83e399545
Carlos <corammun@myuax.com>
2023-11-09 12:26:05 +0100

    3

--- Ejercicios/Ejercicio3.cpp
+++ Ejercicios/Ejercicio3.cpp
@@ -1,13 +1,3 @@
 #include <iostream>
 
-int division(int a, int b){
-    try {
-        a / b;
-        if (b == 0){
-            throw std::runtime_error("No se puede dividir entre 0");
-        }
-    }
-    catch (const std::runtime_error& e){
 
-    }
-}
