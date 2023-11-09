bc6953f0603990ab64814b437de85fe25bd64e4d
Carlos <corammun@myuax.com>
2023-11-09 13:29:49 +0100

    376473

--- Ejercicios/Ejercicios5-8.cpp
+++ Ejercicios/Ejercicios5-8.cpp
@@ -9,16 +9,37 @@ private: //Establecimiento de parametros
     int edad;
     std::string grado;
     std::vector<std::string> asignaturas;//Vector/lista para guardar las asignaturas/materias
+    std::vector<int> calificaciones; //Vector pata notas
 public:
     estudiante(std::string n, int e, std::string g) : nombre(n), edad(e), grado(g) {} //adecuar para la llamada en main
+    double calcular_promedio(){
+        int s = 0;
+        if(calificaciones.empty()){
+            std::cout << "No hay notas o no ha sido evaluado" << std::endl;
+            return 0; // Se imprime si on hay notas
+        }
+        else {
+            for(int calificacion : calificaciones){
+                s += calificacion;
+            }
+            double promedio = static_cast<double>(s) / calificaciones.size();
+            std::cout << "Promedio de calificaciones: " << promedio << std::endl; //Imprime el promedio en pantalla
+            return 0;
+        }
+    }
+
     void mostrar_info(){ // mostrar los datos del estudiante cuando llamado
         std::cout << "Nombre: " << nombre << std::endl;
         std::cout << "Edad: " << edad << std::endl;
         std::cout << "Nombre: " << grado << std::endl;
         mostrar_asignatura(); //Lama a la funcion mostrar asignatura
+        calcular_promedio(); // LLama a la funcion calculat promedio
     }
     void registrar_asignatura(const std::string& asignatura) { //funcion utilizada para registrar asignaturas en el vector
         asignaturas.push_back(asignatura);
+    }
+    void registrar_calificaciones(const std::vector<int>& notas){
+        calificaciones = notas; // funcion patra registrar las noras
     }
     void mostrar_asignatura(){
         if (!asignaturas.empty()) {
@@ -38,5 +59,7 @@ int main(){
     estudiante1.registrar_asignatura("Mates"); //Asignaturas/materias que se registran
     estudiante1.registrar_asignatura("Fisica");
     estudiante1.registrar_asignatura("Programacion");
+    std::vector<int> calificaciones = {10,9,3,5,6}; //Crea un vector conteniendo las notas
+    estudiante1.registrar_calificaciones(calificaciones); //Registra las notas
     estudiante1.mostrar_info(); // Se llama a la funcion mostrar_info
 }
