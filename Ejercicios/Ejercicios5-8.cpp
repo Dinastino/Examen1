bb965d957d29fcc93a69c389bcf57a9c374418c1
Carlos <corammun@myuax.com>
2023-11-09 13:53:02 +0100

    6

--- Ejercicios/Ejercicios5-8.cpp
+++ Ejercicios/Ejercicios5-8.cpp
@@ -3,6 +3,21 @@
 #include <string>
 #include <vector>
 
+class RegistroAsistencia { //Nueva clase para la asistencia
+private:
+    std::string fecha; //Parametros
+    std::string estado;
+
+public:
+    RegistroAsistencia(std::string f, std::string e) : fecha(f), estado(e) {}
+    //LO mismo que con el nombre/edad/grado esta funcion permite recoger los datos del main
+
+    void mostrar_asistencia() { //funcion que se utiliza para mostrar la asistencia
+        std::cout << "Fecha de la clase: " << fecha << std::endl;
+        std::cout << "Estado de asistencia: " << estado << std::endl;
+    }
+};
+
 class estudiante{ //Creacion de la clase estudiante
 private: //Establecimiento de parametros
     std::string nombre;
@@ -10,6 +25,7 @@ private: //Establecimiento de parametros
     std::string grado;
     std::vector<std::string> asignaturas;//Vector/lista para guardar las asignaturas/materias
     std::vector<int> calificaciones; //Vector pata notas
+    std::vector<RegistroAsistencia> asistencia;
 public:
     estudiante(std::string n, int e, std::string g) : nombre(n), edad(e), grado(g) {} //adecuar para la llamada en main
     double calcular_promedio(){
@@ -34,6 +50,7 @@ public:
         std::cout << "Nombre: " << grado << std::endl;
         mostrar_asignatura(); //Lama a la funcion mostrar asignatura
         calcular_promedio(); // LLama a la funcion calculat promedio
+        mostrar_asistencia()
     }
     void registrar_asignatura(const std::string& asignatura) { //funcion utilizada para registrar asignaturas en el vector
         asignaturas.push_back(asignatura);
@@ -41,6 +58,8 @@ public:
     void registrar_calificaciones(const std::vector<int>& notas){
         calificaciones = notas; // funcion patra registrar las noras
     }
+    void registrar_asistencia(const RegistroAsistencia& registro) {
+        asistencia.push_back(registro);
     void mostrar_asignatura(){
         if (!asignaturas.empty()) {
             std::cout << "Asignaturas registradas:" << std::endl;
@@ -61,5 +80,16 @@ int main(){
     estudiante1.registrar_asignatura("Programacion");
     std::vector<int> calificaciones = {10,9,3,5,6}; //Crea un vector conteniendo las notas
     estudiante1.registrar_calificaciones(calificaciones); //Registra las notas
+
+    RegistroAsistencia asistencia1("2023-11-09", "Asistió"); // Introduccnion de asistencias
+    RegistroAsistencia asistencia2("2023-11-10", "Falta");
+    RegistroAsistencia asistencia3("2023-11-11", "Tardanza");
+    RegistroAsistencia asistencia4("2023-11-12", "Falta");
+
+    estudiante1.registrar_asistencia(asistencia1);
+    estudiante1.registrar_asistencia(asistencia2);
+    estudiante1.registrar_asistencia(asistencia1);
+    estudiante1.registrar_asistencia(asistencia2);
+
     estudiante1.mostrar_info(); // Se llama a la funcion mostrar_info
 }
